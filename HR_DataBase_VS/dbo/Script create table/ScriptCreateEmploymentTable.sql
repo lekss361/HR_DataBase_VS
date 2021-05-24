bd2132bd91bb21466c10bef20f4619234b9b5161
creatorQuen <spastime@yandex.ru>
2021-05-24 21:27:26 +0300

    Create EmpoymentTable script

---
+++ HR_DataBase_VS/dbo/Script create table/ScriptCreateEmploymentTable.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [Employment] (
+	WorkerID int NOT NULL,
+	CompanyID int NOT NULL,
+	DivisionID int NOT NULL,
+	ProjectID int NOT NULL,
+	PositionID int NOT NULL,
+	StatusID int NOT NULL,
+  CONSTRAINT [PK_EMPLOYMENT] PRIMARY KEY CLUSTERED
+  (
+  [WorkerID] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF)
+
+)
+GO
