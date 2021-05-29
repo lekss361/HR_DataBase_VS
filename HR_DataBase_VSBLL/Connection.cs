9e0330f7c7983c6b5fd2cbb72404c55bad63932c
Timur <Kotafrakt@gmail.com>
2021-05-29 21:55:43 +0300

    add conection

---
+++ HR_DataBase_VSBLL/Connection.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HR_DataBase_VSBLL
+{
+    public class Connection
+    {
+        private static string _cS = @"Persist Security Info=False;User ID=DevEd;Password=qqq!11;Initial Catalog=Sandbox.Test;Server=80.78.240.16";
+    }
+}
