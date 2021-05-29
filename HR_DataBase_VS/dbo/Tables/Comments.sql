58836f1d36462af26711729349ee8eadc6155c59
Timur <Kotafrakt@gmail.com>
2021-05-29 14:46:45 +0300

    fix table

--- HR_DataBase_VS/dbo/Tables/Comments.sql
+++ HR_DataBase_VS/dbo/Tables/Comments.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Comments] (
     [WorkersID] INT          NOT NULL,
-    [Comment]   NVARCHAR (1) NOT NULL,
+    [Comment]   NVARCHAR (255) NOT NULL,
     [Date]      DATE         NOT NULL,
     CONSTRAINT [Comments_fk0] FOREIGN KEY ([WorkersID]) REFERENCES [dbo].[Workers] ([ID])
 );
