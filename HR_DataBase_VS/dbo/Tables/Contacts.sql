55038713cc2ed9e1a3f575f727a750179a7e98f4
Timur <Kotafrakt@gmail.com>
2021-05-29 14:47:38 +0300

    fix table contacts

--- HR_DataBase_VS/dbo/Tables/Contacts.sql
+++ HR_DataBase_VS/dbo/Tables/Contacts.sql
@@ -2,7 +2,7 @@
     [ID]          INT            NOT NULL,
     [Phone]       DECIMAL (30)   NULL,
     [Email]       NVARCHAR (255) NULL,
-    [Information] NVARCHAR (1)   NOT NULL,
+    [Information] NVARCHAR (255)   NOT NULL,
     CONSTRAINT [PK_CONTACTS] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
