274922ff3cf1d36d3ff22cbb76b475c10bb20634
Frantskevich <orlykino@gmail.com>
2021-05-31 00:39:20 +0300

    Add Position and Division

---
+++ HR_DataBase_VS/dbo/Stored Procedures/Division/AddNewDivision.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[AddNewDivision]
+	@Name nvarchar (255),
+	@CompanyID int,   
+    @ContactID int,
+    @LocationID int,
+    @DirectionsID int
+AS
+	insert [Division] ([Name],[CompanyID], [ContactID], [LocationID], [DirectionsID])
+	values (@Name,CompanyID, @ContactID, @LocationID, @DirectionsID)
---
+++ HR_DataBase_VS/dbo/Stored Procedures/Position/AddNewPosition.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[AddNewPosition]
+	@Name nvarchar (255),
+	@DirectionsID int,
+	@DivisionsID int
+AS
+	INSERT [Positions]([Name], [DirectionsID], [DivisionsID])
+	values (@Name, @DirectionsID, @DivisionsID )
