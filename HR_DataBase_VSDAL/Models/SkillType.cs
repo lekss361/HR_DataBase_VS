2a5115ae80d851e56cbd74a3716b0303a69698b6
Eugene <Zhekul.90@gmail.com>
2021-05-31 02:52:06 +0300

    add model

---
+++ HR_DataBase_VSDAL/Models/SkillType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_DataBase_VSDAL.Models
+{
+    public class SkillType
+    {
+        public int SkillTypeID { get; set; }
+        public string Name { get; set; }
+    }
+}
