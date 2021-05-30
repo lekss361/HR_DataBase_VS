9f4f153c96468a3bf5728d9f9f6cef31c251b4b5
Eugene <Zhekul.90@gmail.com>
2021-05-31 02:08:39 +0300

    add model

---
+++ HR_DataBase_VSDAL/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_DataBase_VSDAL.Models
+{
+    public class Location
+    {
+        public int LocationIndex { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int HouseNumber { get; set; }
+        public int ApartmentNumber { get; set; }
+    }
+}
