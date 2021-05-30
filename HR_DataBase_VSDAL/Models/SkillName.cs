397ec998d62c89b8bbefb4ef75339c97a6c28ed4
Timur <Kotafrakt@gmail.com>
2021-05-31 01:21:24 +0300

    add

---
+++ HR_DataBase_VSDAL/Models/SkillName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_DataBase_VSDAL.Models
+{
+    public class SkillName
+    {
+        public string Skill { get; set; }
+        public int SkillTypeID { get; set; }
+    }
+}
