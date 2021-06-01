73e7bffeba17a3f729a52af7597ff8b7ad7132f3
Franskevich <oorlykino@gmail.com>
2021-06-01 15:54:51 +0300

    Complete DivisionsProcedure

---
+++ HR_DataBase_VS/HR_DataBase_VSDB/Stored Procedures/Division/DeleteDivisionByIndex.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [HR_DataBase_VSDB].[DeleteDivisionByIndex]
+	@id int
+	AS
+	DELETE FROM [Divisions]
+	WHERE [id] = @id 
---
+++ HR_DataBase_VS/HR_DataBase_VSDB/Stored Procedures/Division/UpdateDivisionByID.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [HR_DataBase_VSDB].[UpdateDivisionByID]
+	@id int	              ,
+	@Name  NVARCHAR (255) ,
+    @CompanyID   INT      ,      
+    @ContactID   INT      ,      
+    @LocationID  INT      ,      
+    @DirectionsID INT
+    AS
+    UPDATE [Divisions]
+	set [Name] = @Name,
+    [CompanyID] = @CompanyID,
+    [ContactID] = @ContactID,
+    [LocationID] = @LocationID,
+    [DirectionsID] = @DirectionsID
+    WHERE [id] = @id
