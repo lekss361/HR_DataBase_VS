1f6235f94fba3f73fc0539cab8898179f0ffb0a3
Timur <Kotafrakt@gmail.com>
2021-06-03 01:46:50 +0300

    DeleteWorkersSkillMapByWorkersIDandSkillMapsID

--- HR_DataBase_VS/HR_DataBase_VSDB/Stored Procedures/WorkersSkillMaps/DeleteWorkersSkillMapByID.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE PROCEDURE [HR_DataBase_VSDB].[DeleteWorkersSkillMapByID]
-	@WorkerID int,
-    @SkillMapsID int
-AS
-	DELETE from [WorkersSkillMaps] 
-	WHERE [WorkersID] = @WorkerID
-	--WHERE [SkillsMapsID] = @SkillMapID 
