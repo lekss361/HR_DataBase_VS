5602ef886291f3f89b928a3bace46b67575e1ecd
Timur <Kotafrakt@gmail.com>
2021-06-04 02:48:22 +0300

    add

---
+++ HR_DataBase_VSBLL/ModelsBLL/Companies.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using HR_DataBase_VSDAL.Models;
+using Dapper;
+using System.Data.SqlClient;
+
+
+namespace HR_DataBase_VSBLL
+{
+    class Companies
+    {
+        public static List<CompaniesDTO> GetAllCompanies()
+        {
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=HR_DataBase_VSDB;Integrated Security=True";
+            string quary = "exec GetAllCompanies";
+            List<CompaniesDTO> companies = new List<CompaniesDTO>();
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                companies = connection.Query<CompaniesDTO>(quary).AsList<CompaniesDTO>();
+            }
+            return companies;
+        }
+    }
+}
