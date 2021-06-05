5c7dd1861e41707244acfd4f9d3ee8037d2a1e2d
Eugene <Zhekul.90@gmail.com>
2021-06-05 19:29:17 +0300

    add dapper

---
+++ HR_DataBase_VSDAL/Dapper/LocationDapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using HR_DataBase_VSDAL.Models;
+
+
+namespace HR_DataBase_VSDAL.Dapper
+{
+    public class LocationDapper
+    {
+        /// <summary>
+        /// Делаем запись в Базу Данных через хранимую процедуру
+        /// </summary>
+        /// <param name="locationDTO"></param>
+        /// <returns></returns>
+        public void AddNewLocation(LocationDTO locationDTO)
+        {
+            //List<LocationDTO> locations = new List<LocationDTO>();
+            SqlCommand command = new SqlCommand();
+
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=HR_DataBase_VSDB;Integrated Security=True";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@Country", locationDTO.Country);
+            command.Parameters.AddWithValue("@City", locationDTO.City);
+            command.Parameters.AddWithValue("@Street", locationDTO.Street);
+            command.Parameters.AddWithValue("@HouseNumber", locationDTO.HouseNumber);
+            command.Parameters.AddWithValue("@ApartmentNumber", locationDTO.ApartmentNumber);
+
+            command.CommandText = "exec AddLocation";
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Query<LocationDTO>(command.CommandText);
+            }
+        }
+    }
+}
