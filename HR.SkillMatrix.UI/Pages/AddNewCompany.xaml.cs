5be27b5a42e1ab814c2e89dc0324eb0a9deb1e7a
Eugene <Zhekul.90@gmail.com>
2021-06-05 15:15:39 +0300

    rename model Companies->Company

--- HR.SkillMatrix.UI/Pages/AddNewCompany.xaml.cs
+++ HR.SkillMatrix.UI/Pages/AddNewCompany.xaml.cs
@@ -35,7 +35,7 @@ namespace HR.SkillMatrix.UI.Pages
         /// <param name="e"></param>
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            Companies companies = new Companies();
+            HR_DataBase_VSBLL.Models.Company companies = new HR_DataBase_VSBLL.Models.Company();
 
             companies.Name = textBoxName.Text;
             companies.Phone = textBoxPhone.Text;
