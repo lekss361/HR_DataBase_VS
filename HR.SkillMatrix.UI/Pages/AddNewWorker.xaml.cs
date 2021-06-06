80471400c06ac69a32954b8d09f79594c3d6f8e3
Timur <Kotafrakt@gmail.com>
2021-06-06 17:27:27 +0300

    change ui add button and link

---
+++ HR.SkillMatrix.UI/Pages/AddNewWorker.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HR_DataBase_VSBLL.Models;
+
+namespace HR.SkillMatrix.UI.Pages
+{
+    /// <summary>
+    /// Interaction logic for Worker.xaml
+    /// </summary>
+    public partial class AddNewWorker : Page
+    {
+        private readonly MainWindow _mainWindow;
+        public AddNewWorker(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            CreateNewPage createNewPage = new CreateNewPage(_mainWindow);
+            _mainWindow.Content = createNewPage;
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void BoxContacts_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 45 };
+            foreach (object ss in numbers)
+            { 
+                BoxContacts.Items.Add(ss + "ss");
+            }
+
+            //Button addlocations = new Button();
+            //addlocations.Height = 100;
+            //addlocations.Width = 100;
+            //addlocations.Content = "sss";
+            //addlocations.Click += AddLocation_OnClick;
+        }
+
+        private void AddLocation_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddLocationMenu locationMenu = new AddLocationMenu(_mainWindow);
+            _mainWindow.Content = locationMenu;
+        }
+    }
+}
