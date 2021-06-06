a837ffba61bcaac2f27f7612296bb45ad3da2ab7
Eugene <Zhekul.90@gmail.com>
2021-06-06 11:17:27 +0300

    add LocationDapper GetLocationByID

--- HR_DataBase_VSDAL/Dapper/LocationDapper.cs
+++ HR_DataBase_VSDAL/Dapper/LocationDapper.cs
@@ -13,22 +13,46 @@ namespace HR_DataBase_VSDAL.Dapper
 {
     public class LocationDapper
     {
+        LocationDTO locationDTO;
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HRDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
+
+
         /// <summary>
         /// Делаем запись в Базу Данных через хранимую процедуру
         /// </summary>
         /// <param name="locationDTO"></param>
-        /// <returns></returns>
         public void AddNewLocation(LocationDTO locationDTO)
         {
-            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HRDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
-            string tmp1= "exec [HR_DataBase_VSDB].[AddLocation]";
+            string tmp1 = "exec [HR_DataBase_VSDB].[AddLocation]";
             string tmp2 =
-                $" N'{locationDTO.LocationIndex}', N'{locationDTO.Country}', N'{locationDTO.City}', N'{locationDTO.Street}', N'{locationDTO.HouseNumber}', N'{locationDTO.ApartmentNumber}'";
+                $"N'{locationDTO.LocationIndex}', " +
+                $"N'{locationDTO.Country}', " +
+                $"N'{locationDTO.City}', " +
+                $"N'{locationDTO.Street}', " +
+                $"N'{locationDTO.HouseNumber}', " +
+                $"N'{locationDTO.ApartmentNumber}'";
 
             using (IDbConnection connection = new SqlConnection(connectionString))
             {
                 connection.Query<LocationDTO>(@$"{tmp1}{tmp2}");
             }
+        }
+
+        /// <summary>
+        /// Находим запись по ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>LocationDTO</returns>
+        public LocationDTO GetLocationByID(int id)
+        {
+            string tmp1 = "exec [HR_DataBase_VSDB].[GetLocationByID]";
+            string tmp2 =
+                $"N'{locationDTO.ID = id}'";
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                locationDTO = (LocationDTO)connection.Query<LocationDTO>(@$"{tmp1}{tmp2}");
+            }
+            return locationDTO;
         }
     }
 }
