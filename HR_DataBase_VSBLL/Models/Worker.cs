edf48fbab20c1fb3ce8177fd8029f55f29a9b7ba
Timur <Kotafrakt@gmail.com>
2021-06-07 15:23:12 +0300

    add Worker model

---
+++ HR_DataBase_VSBLL/Models/Worker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_DataBase_VSBLL.Models
+{
+    class Worker
+    {
+    }
+}
