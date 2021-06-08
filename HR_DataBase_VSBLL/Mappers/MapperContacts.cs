5b3788b1931e65d42a2254ef3cbd6e1af3d28076
Eugene <Zhekul.90@gmail.com>
2021-06-08 05:51:16 +0300

    add MapperContacts

---
+++ HR_DataBase_VSBLL/Mappers/MapperContacts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HR_DataBase_VSBLL.Models;
+using HR_DataBase_VSDAL.Models;
+using HR_DataBase_VSDAL.Dapper;
+
+namespace HR_DataBase_VSBLL.Mappers
+{
+    public class MapperContacts
+    {
+        ContactsDTO contactsDTO = new ContactsDTO();
+        DapperContact dapper = new DapperContact();
+
+        /// <summary>
+        /// поиск данных по ID
+        /// </summary>
+        /// <param name="ID">ID записи</param>
+        /// <returns>DTO записи из БД</returns>
+        public ContactsDTO GetLocationByID(int ID)
+        {
+            contactsDTO = dapper.GetContactByID(ID);
+            return contactsDTO;
+        }
+
+        /// <summary>
+        /// Добавляем новую запись в БД
+        /// </summary>
+        /// <param name="model">Модель</param>
+        public void AddNewLocation(Contacts model)
+        {
+            contactsDTO = MapModelToDTO(model);
+            dapper.AddNewContact(contactsDTO);
+        }
+
+        /// <summary>
+        /// Mapper моделей UI в DTO
+        /// </summary>
+        /// <param name="model"></param>
+        private ContactsDTO MapModelToDTO(Contacts model)
+        {
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Contacts, ContactsDTO>()
+            .ForMember(dest => dest.Phone, option => option.MapFrom(source => source.Phone))
+            .ForMember(dest => dest.Email, option => option.MapFrom(source => source.Email))
+            .ForMember(dest => dest.Information, option => option.MapFrom(source => source.Information)));
+
+            Mapper mapper = new Mapper(config);
+
+            contactsDTO = mapper.Map<ContactsDTO>(model);
+            return contactsDTO;
+        }
+
+    }
+}
