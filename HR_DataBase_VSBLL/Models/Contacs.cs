b2ca395cad058cf5b5b35dc4890d3522f877ee99
Eugene <Zhekul.90@gmail.com>
2021-06-08 03:56:43 +0300

    make field Phone nvarchar (15)

--- HR_DataBase_VSBLL/Models/Contacs.cs
+++ HR_DataBase_VSBLL/Models/Contacs.cs
@@ -8,7 +8,7 @@ namespace HR_DataBase_VSBLL.Models
 {
     public class Contacs
     {
-        public decimal Phone { get; set; }
+        public string Phone { get; set; }
         public string Email { get; set; }
         public string Information { get; set; }
 
--- HR_DataBase_VSDAL/DTO/Contacts.cs
+++ HR_DataBase_VSDAL/DTO/Contacts.cs
@@ -4,7 +4,7 @@ namespace HR_DataBase_VSDAL.Models
     class Contacts
     {
         public int id { get; set; }
-        public decimal Phone { get; set; }
+        public string Phone { get; set; }
         public string Email { get; set; }
         public string Information { get; set; }
     }
