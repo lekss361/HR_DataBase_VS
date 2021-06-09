98408a89772d9db050b3ee386762772661abbb1f
Eugen <phlosof@gmail.com>
2021-06-09 23:48:31 +0300

    AddProjectsWithDirectionName

---
+++ HR_DataBase_VSBLL/Models/ProjectsWithDirectionName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_DataBase_VSBLL.Models
+{
+   public class ProjectsWithDirectionName
+    {
+        public string Name { get; set; }
+        public string DirectionName { get; set; }
+        public string Information { get; set; }
+    }
+}
---
+++ HR_DataBase_VSDAL/DTO/ProjectsWithDirectionNameDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_DataBase_VSDAL.Models
+{
+   public class ProjectsWithDirectionNameDTO
+    {
+        public string Name { get; set; }
+        public string DirectionName { get; set; }
+        public string Information { get; set; }
+    }
+}
