598b5dfcd71d8188684c47fa0397950e557c811a
Franskevich <oorlykino@gmail.com>
2021-06-09 15:23:20 +0300

    List of Companies

--- HR_DataBase_VSDAL/Dapper/DapperCompanies.cs
+++ HR_DataBase_VSDAL/Dapper/DapperCompanies.cs
@@ -13,11 +13,16 @@ namespace HR_DataBase_VSDAL.Dapper
 {
     public class CompanyDapper
     {
+        //CompaniesDTO companiesDTO;
+        List<CompaniesDTO> ListDTO = new List<CompaniesDTO>();
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HRDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
+
         /// <summary>
         /// Делаем запись в Базу Данных через хранимую процедуру
         /// </summary>
         /// <param name="companiesDTO"></param>
         /// <returns></returns>
+
         public void AddNewCompany(CompaniesDTO companiesDTO)
         {
             string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HRDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
@@ -29,6 +34,24 @@ namespace HR_DataBase_VSDAL.Dapper
             {
                 connection.Query<CompaniesDTO>(@$"{tmp1}{tmp2}");
             }
+        }
+
+        /// <summary>
+        /// Находим запись по ID
+        /// </summary>
+        /// <returns>DTO записи из БД</returns>
+        
+        public List<CompaniesDTO> GetAllCompanies()
+        {
+            string tmp1 = "exec [HR_DataBase_VSDB].[GetAllCompanies]";
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                ListDTO = connection
+                    .Query<CompaniesDTO>(@$"{tmp1}")
+                    .AsList<CompaniesDTO>();
+            }
+            return ListDTO;
         }
     }
 }
