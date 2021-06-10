6f1b879efab2226b825efc4a14287ec412971b91
Timur <Kotafrakt@gmail.com>
2021-06-10 21:55:48 +0300

    add buttons

--- HR.SkillMatrix.UI/Pages/SearchForEmployee.xaml.cs
+++ HR.SkillMatrix.UI/Pages/SearchForEmployee.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HR_DataBase_VSBLL.Mappers;
+using HR_DataBase_VSBLL.Models;
 
 namespace HR.SkillMatrix.UI.Pages
 {
@@ -30,6 +32,28 @@ namespace HR.SkillMatrix.UI.Pages
         {
             MainMenu mainMenu = new MainMenu(_mainWindow);
             _mainWindow.Content = mainMenu;
+        }
+
+        private void Companies_OnInitialized(object? sender, EventArgs e)
+        {
+            MapperCompanies mapperCompanies = new MapperCompanies();
+            List<HR_DataBase_VSBLL.Models.Company> sss = mapperCompanies.GetAllCompanies();
+            foreach (var C in sss)
+            {   
+                Companies.Items.Add(C.Name);
+            }
+
+            Button addlocations = new Button();
+            addlocations.Height = 100;
+            addlocations.Width = 100;
+            addlocations.Content = "sss";
+            addlocations.Click += AddCompany_OnClick;
+        }
+
+        private void AddCompany_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddNewCompany addNewCompany = new AddNewCompany(_mainWindow);
+            _mainWindow.Content = addNewCompany;
         }
     }
 }
