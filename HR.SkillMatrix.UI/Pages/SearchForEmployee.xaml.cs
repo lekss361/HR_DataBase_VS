5899e2cdef165700192c09fbf6b761dd01dde69b
Timur <Kotafrakt@gmail.com>
2021-06-10 17:28:43 +0300

    Ad ui

---
+++ HR.SkillMatrix.UI/Pages/SearchForEmployee.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HR.SkillMatrix.UI.Pages
+{
+    /// <summary>
+    /// Interaction logic for SearchForEmployee.xaml
+    /// </summary>
+    public partial class SearchForEmployee : Page
+    {
+        private readonly MainWindow _mainWindow;
+        public SearchForEmployee(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu(_mainWindow);
+            _mainWindow.Content = mainMenu;
+        }
+    }
+}
