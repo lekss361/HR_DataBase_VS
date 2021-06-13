95f284227036675ff9795dcb4a8685f236d31232
Eugene <Zhekul.90@gmail.com>
2021-06-14 02:23:15 +0300

    add MapperWorker SearchWorkersBySameParams

--- HR_DataBase_VSBLL/Mappers/MapperWorker.cs
+++ HR_DataBase_VSBLL/Mappers/MapperWorker.cs
@@ -12,36 +12,38 @@ namespace HR_DataBase_VSBLL.Mappers
 {
     public class MapperWorker
     {
-        int id;
+        int Id;
+
+        WorkersDTO workersDTO = new WorkersDTO();
+        List<WorkersDTO> workersDTOs = new List<WorkersDTO>();
+        List<Worker> workers = new List<Worker>();
+
         /// <summary>
         /// Mapper моделей UI в DTO
         /// </summary>
         /// <param name="worker"></param>
         public int MapToWorkersDTO(Worker worker)
         {
-
-            WorkersDTO workersDTO = new WorkersDTO();
-
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<Worker, WorkersDTO>()
-                .ForMember(dest => dest.BirthDay, option => option.MapFrom(source => source.BirthDay))
-                .ForMember(dest => dest.ContactID, option => option.MapFrom(source => source.ContactID))
-                .ForMember(dest => dest.Education, option => option.MapFrom(source => source.Education))
-                .ForMember(dest => dest.FirstName, option => option.MapFrom(source => source.FirstName))
-                .ForMember(dest => dest.LastName, option => option.MapFrom(source => source.LastName))
-                .ForMember(dest => dest.Patronymic, option => option.MapFrom(source => source.Patronymic))
-                .ForMember(dest => dest.LocationID, option => option.MapFrom(source => source.LocationID))
-                .ForMember(dest => dest.Sex, option => option.MapFrom(source => source.Sex))
-                .ForMember(dest => dest.StatusID, option => option.MapFrom(source => source.StatusID))
-                .ForMember(dest => dest.Hobby, option => option.MapFrom(source => source.Hobby))
-                .ForMember(dest => dest.PositionID, option => option.MapFrom(source => source.PositionID))
-                .ForMember(dest => dest.DivisionID, option => option.MapFrom(source => source.DivisionID)));
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Worker, WorkersDTO>());
 
             Mapper mapper = new Mapper(config);
             DapperWorker dapper = new DapperWorker();
 
             workersDTO = mapper.Map<WorkersDTO>(worker);
-            id=dapper.AddNewWorker(workersDTO);
-            return id;
+            Id=dapper.AddNewWorker(workersDTO);
+            return Id;
+        }
+
+        public List<Worker> SearchWorkersBySameParams(Worker worker)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Worker, WorkersDTO>());
+            var configListDTOToListModel = new MapperConfiguration(cfg => cfg.CreateMap<List<WorkersDTO>, List<Worker>>());
+           
+            Mapper mapper = new Mapper(config);
+            Mapper mapperList = new Mapper(configListDTOToListModel);
+            DapperWorker dapper = new DapperWorker();
+            workersDTOs =  dapper.SearchWorkerBySameParam(mapper.Map<WorkersDTO>(worker));
+            return mapperList.Map<List<Worker>>(workersDTOs);
         }
     }
 }
