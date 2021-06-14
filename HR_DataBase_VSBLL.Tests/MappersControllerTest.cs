32eb37931e53b819875ee4c8ebc796d09fe8f8a9
Eugene <Zhekul.90@gmail.com>
2021-06-14 10:17:22 +0300

    add folders Expected, Requests

---
+++ HR_DataBase_VSBLL.Tests/MappersControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Moq;
+
+namespace HR_DataBase_VSBLL.Tests
+{
+    public class MappersControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
