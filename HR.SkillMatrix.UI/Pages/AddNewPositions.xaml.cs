f04aec64742d2384da2b638af8bce7fb28a8d07c
Sergey <tootoo97@gmail.com>
2021-06-14 15:25:45 +0300

    Add PositionUi

---
+++ HR.SkillMatrix.UI/Pages/AddNewPositions.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HR.SkillMatrix.UI.Windows;
+using HR_DataBase_VSBLL.Models;
+using HR_DataBase_VSBLL.Mappers.ModelsToDTO;
+
+namespace HR.SkillMatrix.UI.Pages
+{
+    /// <summary>
+    /// Interaction logic for AddNewPositions.xaml
+    /// </summary>
+    public partial class AddNewPositions : Page
+    {
+        private readonly MainWindow _mainWindow;
+        public AddNewPositions(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            CreateNewPage createNewPage = new CreateNewPage(_mainWindow);
+            _mainWindow.Content = createNewPage;
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
