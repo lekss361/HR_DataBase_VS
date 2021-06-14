2ec04282b93cba3e43d851880ae235f296154397
Aleks <alexandro361@gmail.com>
2021-06-14 15:56:06 +0300

    1

---
+++ HR_DataBase_VSBLL.Tests/Requests/MapControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_DataBase_VSBLL.Tests.Requests
+{
+    class MapControllerTests
+    {
+    }
+}
